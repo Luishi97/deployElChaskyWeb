9ef372567d1777f83645ade59e1920007bcdef8d
Luis Sardinas <luisin_sardinas@hotmail.com>
2020-08-09 11:01:50 -0400

    Ãe-report deploy

---
+++ precache-manifest.434b41a38a6f250b04c28b2d9ed49a40.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3e678eed3b91a1f0760d4125bf26b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e043a85e7204db777f41",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7a753b4aeb99947a6b76",
+    "url": "/static/css/3.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "e043a85e7204db777f41",
+    "url": "/static/js/2.6da9b233.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6da9b233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a753b4aeb99947a6b76",
+    "url": "/static/js/3.275baefd.chunk.js"
+  },
+  {
+    "revision": "76ca09a658592aa65cc73fe8cd531863",
+    "url": "/static/js/3.275baefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a809090d83846dbd9b",
+    "url": "/static/js/4.a9f1b2ca.chunk.js"
+  },
+  {
+    "revision": "c07e7a7f6d39c0c16964387eea8a75cb",
+    "url": "/static/js/4.a9f1b2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea15aadf9f3989b5b8f",
+    "url": "/static/js/5.276e4058.chunk.js"
+  },
+  {
+    "revision": "fb9a86ed5d63e484c920",
+    "url": "/static/js/6.0d752787.chunk.js"
+  },
+  {
+    "revision": "57b8bb4e3b0c699ade08",
+    "url": "/static/js/main.c82edab7.chunk.js"
+  },
+  {
+    "revision": "5355e5a48eadfaedf291",
+    "url": "/static/js/runtime-main.c246af1b.js"
+  }
+]);
